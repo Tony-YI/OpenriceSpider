195462367e4a306c7c3b092e4f7588ae63ae0c1d
Edward <huangxx_2155@qq.com>
2014-04-16 21:27:41 +0800

    add with data.xlsx and raw data

--- openrice/openrice/analyzer.py
+++ openrice/openrice/analyzer.py
@@ -3,7 +3,8 @@
 import sys;
 import json;
 import xlsxwriter;
-import my_tool;
+import time;
+import datetime;
 
 def read_file_as_string(filename):
 	file_string = "";
@@ -35,10 +36,14 @@ file_list = [];
 DIR = "spiders/"
 for count in range(0,int(number_of_file)):
 	file_list.append("data_" + str(int(start_at) + count * 50) );
+file_list.append("data_2355000");
+file_list.append("data_2355050");
+file_list.append("data_2355100");
+file_list.append("data_2355150");
 print str(file_list);
 
-workbook = xlsxwriter.Workbook('hello.xlsx')
-worksheet = workbook.add_worksheet()
+workbook = xlsxwriter.Workbook('data.xlsx');
+worksheet = workbook.add_worksheet();
 
 count_row = 1;
 worksheet.write_string(count_row,0,"comment_id");
@@ -48,15 +53,19 @@ worksheet.write_string(count_row,3,"date");
 worksheet.write_string(count_row,4,"ratio");
 count_row = count_row + 1;
 
-for count in range(0,int(number_of_file)):
+date_format = workbook.add_format({'num_format':"dd/mm/yy"});
+
+for count in range(0,int(number_of_file) + 4):
+	print "file " + DIR + file_list[count];
 	input_string = read_file_as_string(DIR + file_list[count]);	
 	record_json = json.loads(input_string);
 	for count_record in range(0,len(record_json)):
 		worksheet.write_number(count_row,0,int(record_json[count_record]["comment_id"]));
 		worksheet.write_number(count_row,1,int(record_json[count_record]["num_of_recom"]));
 		worksheet.write_number(count_row,2,int(record_json[count_record]["num_of_view"]));
-		worksheet.write_date(count_row,3,record_json[count_record]["date"]);
+		#worksheet.write_date(count_row,3,record_json[count_record]["date"]);
+		date_time = datetime.datetime.strptime(record_json[count_record]["date"],'%Y-%m-%d');
+		worksheet.write_datetime(count_row,3,date_time,date_format);
 		worksheet.write_number(count_row,4,float(record_json[count_record]["ratio"]));
 		count_row = count_row + 1;
-
 workbook.close();
